ff2453f3f29bbecb345895c4b5a1cd72f474b3f2
Muftyj <muftymenky17@gmail.com>
2026-09-13 08:45:50 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,20 @@
+/*
+Name : James Gona Mkutano
+Reg No : CT100/G/30611/26
+Description : This is a C program that displays all academic units offered this semester.
+*/
+
+#include<stdio.h>
+
+int main() {
+printf("Units offered this semester\n");
+ printf("1.Fundamentals of Digital Technology and Artificial Intelligence\n");
+ printf("2.Introduction to Computer Applications\n");
+ printf("3.Essential of Accounting\n");
+ printf("4.Computer Systems and Organization\n");
+ printf("5.Analogue Electronics\n");
+ printf("6.Introduction to Programming and Algorithms\n");
+ printf("7.Mathematics for Science\n");
+ printf("8.Communication and Information Literacy Skills");
+ return 0;
+ }
